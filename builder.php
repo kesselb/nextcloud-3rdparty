3eda5233a624b2bfa936e001488e040c75ddc57f
Daniel Kesselberg <mail@danielkesselberg.de>
2021-01-10 17:05:42 +0100

    Virtual package for Nextcloud 25
    Signed-off-by: Daniel Kesselberg <mail@danielkesselberg.de>

--- builder.php
+++ builder.php
@@ -1,19 +1,26 @@
 <?php
 
-$branchToVersion = [
-    'master'   => 'dev-master',
-    'stable20' => '20',
-    'stable19' => '19',
-    'stable18' => '18',
+$branches = [
+    'master',
+    'stable24',
+    'stable23',
+    'stable22',
+    'stable21',
+];
+
+$excludePackages = [
+    'symfony/event-dispatcher-contracts',
+    'symfony/service-contracts',
+    'symfony/translation-contracts',
 ];
 
 $branch = $argv[1] ?? null;
 
-if ($branch === null || ! isset($branchToVersion[$branch])) {
-    throw new \RuntimeException('Invalid branch "' . $branch . '"');
+if ($branch === null || !in_array($branch, $branches, true)) {
+    throw new \RuntimeException('Invalid branch "'.$branch.'"');
 }
 
-$contents = file_get_contents('https://raw.githubusercontent.com/nextcloud/3rdparty/' . $branch . '/composer.lock');
+$contents = file_get_contents('https://raw.githubusercontent.com/nextcloud/3rdparty/'.$branch.'/composer.lock');
 
 try {
     $data = json_decode($contents, true, 512, JSON_THROW_ON_ERROR);
@@ -22,22 +29,29 @@ try {
 }
 
 $packages = $data['packages'];
-$replace  = [];
+$replace = [];
 
 foreach ($packages as $package) {
-    $replace[$package['name']] = $package['version'];
+    $packageName = strtolower($package['name']);
+    if (in_array($packageName, $excludePackages, true)) {
+        continue;
+    }
+    $replace[$packageName] = $package['version'];
 }
 
-$composer         = [];
+$composer = [];
 $composer['name'] = 'kesselb/nextcloud-3rdparty';
 $composer['type'] = 'metapackage';
+$composer['license'] = 'MIT';
 if (isset($data['platform']['php'])) {
     $composer['require'] = ['php' => $data['platform']['php']];
 }
 $composer['replace'] = $replace;
+$composer['non-feature-branches'] = ['main', 'stable*'];
+
 
 try {
-    file_put_contents('composer.json', json_encode($composer, JSON_THROW_ON_ERROR | JSON_PRETTY_PRINT));
+    file_put_contents('composer.json', json_encode($composer, JSON_THROW_ON_ERROR  | JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT));
 } catch (JsonException $e) {
     throw new \RuntimeException($e->getMessage());
 }
